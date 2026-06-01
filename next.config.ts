208804f1f5047742e1128018a034cdb46cea84dc
Deepanshu Thakur <deepanshuthakur172006@gmail.com>
2026-06-01 23:19:05 +0530

    chore: remove ESLint configuration and enable build-time type and lint checking

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: any = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
